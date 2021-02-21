711a084aed7768afdd0d62572b04801fd7ac98d4
jcinterrante <jcinterrante@uchicago.edu>
2021-02-21 17:53:26 -0600

    Revised graphs and fine-tuned sentiment analysis in Q1

--- question1.R
+++ question1.R
@@ -15,7 +15,7 @@ bis_ngram_tokens <- unnest_tokens(text_df, word_tokens, text, token = "ngrams", 
 count(bis_word_tokens, word_tokens, sort = TRUE)
 stop_words
 
-bis_no_stop_words <- bis_word_tokens %>% 
+bis_no_stop_words <- bis_word_tokens %>%
   anti_join(stop_words, by = c("word_tokens" = "word")) %>%
   count(word_tokens, sort = TRUE)
 
@@ -24,52 +24,82 @@ sentiment_nrc <- get_sentiments("nrc")
 sentiment_afinn <- get_sentiments("afinn")
 sentiment_bing <- get_sentiments("bing")
 
-for(s in c("nrc", "afinn", "bing")){
+for (s in c("nrc", "afinn", "bing")) {
   bis_no_stop_words <- bis_no_stop_words %>%
     left_join(get_sentiments(s), by = c("word_tokens" = "word")) %>%
-           plyr::rename(replace = c(sentiment = s, value = s), warn_missing = FALSE)
+    plyr::rename(replace = c(sentiment = s, value = s), warn_missing = FALSE)
 }
-
-ggplot(data = filter(no_stop_words_bis, !is.na(nrc)))+
-  geom_histogram(aes(nrc), stat = "count") +
-  scale_x_discrete(guide = guide_axis(angle = 45)) +
-  labs(title = "BIS Sentiment (NRC)")
-
 
 getStemLanguages()
 
 bis_udp <- udpipe(data, "english")
 bis_udp$stem <- wordStem(bis_udp$token, language = "porter")
 
-bis_udp_output <- select(bis_udp, "token", "stem", "lemma", "upos")%>%
-  filter(!upos %in% c("PART", "PUNCT", "CCONJ", "SYM", "NUM", "ADP", "AUX", "DET", "PRON", "X", "SCONJ"))
+not_list <- bis_udp %>%
+  filter(str_to_lower(lemma) == "not")
 
-bis_udp_no_stop_words <- bis_udp_output %>% 
-  anti_join(stop_words, by = c("token" = "word")) %>%
-  count(lemma, sort = TRUE)
+bis_udp_output <- select(bis_udp, "doc_id", "term_id", "token", "stem", "lemma", "upos") %>%
+  filter(!upos %in% c("PART", "PUNCT", "CCONJ", "SYM", "NUM", "ADP", "AUX", "DET", "PRON", "X", "SCONJ")) %>%
+  mutate_if(is.character, str_to_lower)
 
-for(s in c("nrc", "afinn", "bing")){
-  bis_udp_no_stop_words <- bis_udp_no_stop_words%>%
+# Remove stop words, and also remove the words "debt" and "outstanding." I found
+# that these terms were highly influential on the sentiment scores, even though
+# they were being used in a purely technical, value-neutral sense. "Debt" in the
+# sense of "sovereign debt" can be bad if it is unsustainable, but it can also
+# be good, as in the case of stimulus spending. Similarly, "outstanding" arises
+# in the context of "outstanding obligations," rather than as a synonym for
+# "good."
+
+bis_udp_no_stop_words <- bis_udp_output %>%
+  anti_join(stop_words, by = c("lemma" = "word")) %>%
+  count(lemma, sort = TRUE) %>%
+  filter(!lemma %in% c("debt", "outstanding"))
+
+for (s in c("nrc", "afinn", "bing")) {
+  bis_udp_no_stop_words <- bis_udp_no_stop_words %>%
     left_join(get_sentiments(s), by = c("lemma" = "word")) %>%
     plyr::rename(replace = c(sentiment = s, value = s), warn_missing = FALSE)
 }
 
-ggplot(data = filter(bis_udp_no_stop_words, !is.na(bing)))+
-  geom_histogram(aes(bing), stat = "count") +
-  scale_x_discrete(guide = guide_axis(angle = 45)) +
-  labs(title = "BIS Sentiment (Bing)")
+bis_udp_no_stop_words <- bis_udp_no_stop_words %>%
+  mutate(bing = if_else(bing == "positive", 1, -1))
 
-ggplot(data = filter(bis_udp_no_stop_words, !is.na(nrc)))+
-  geom_histogram(aes(nrc), stat = "count") +
-  scale_x_discrete(guide = guide_axis(angle = 45)) +
-  labs(title = "BIS Sentiment (NRC)")
+summary(bis_udp_no_stop_words)
 
+bing_graph_data <- bis_udp_no_stop_words %>%
+  group_by(lemma, n, bing) %>%
+  summarize() %>%
+  mutate(bing_score = n * bing)
+
+ggplot(filter(bing_graph_data, abs(bing_score) > 1)) +
+  geom_bar(aes(
+    y = reorder(lemma, bing_score), x = bing_score,
+    fill = factor(bing_score > 0)
+  ),
+  stat = "identity"
+  ) +
+  labs(
+    title = "Overall Contribution to Sentiment Score by Word (Bing)",
+    x = "Bing Score", y = ""
+  ) +
+  theme(legend.position = "none")
 
 summary <- bis_udp_no_stop_words %>%
-  group_by(nrc)%>%
-  summarize(count = sum(n, na.rm = TRUE))%>%
-  filter(!is.na(nrc))%>%
-  pivot_wider(names_from = "nrc", values_from = count)
+  group_by(nrc) %>%
+  summarize(count = sum(n, na.rm = TRUE)) %>%
+  filter(!is.na(nrc)) %>%
+  pivot_wider(names_from = "nrc", values_from = count) %>%
+  select(positive, joy, anticipation, disgust, anger, negative, sadness, fear, surprise, trust)
+
 # https://www.r-graph-gallery.com/142-basic-radar-chart.html
-data <- rbind(rep(250,10) , rep(0,10) , summary)
-radarchart(data)
+data <- rbind(rep(250, 10), rep(0, 10), summary)
+radarchart(data,
+  pfcol = rgb(0.2, 0.5, 0.5, 0.4),
+  pcol = rgb(0.2, 0.5, 0.5, 0.9),
+  plwd = 2,
+  axislabcol = "gray",
+  axistype = 1,
+  cglcol = "grey", cglty = 1, cglwd = 0.8, caxislabels = seq(0, 250, 62.5),
+  title = "Sentiments in the December 2020 BIS (NRC)",
+  vlcex = 1
+)
--- question2.R
+++ question2.R
@@ -5,7 +5,8 @@ library(udpipe)
 library(fmsb)
 library(rvest)
 library(stringr)
-
+library(lubridate)
+getStemLanguages()
 
 request_page <- function(url, delay = 0.1){
     print(url)
@@ -22,13 +23,93 @@ request_page <- function(url, delay = 0.1){
     
 }
 
-#analyze_sentiments
+generate_radar_plots <- function(df){
+    
+    summary <- df %>%
+        group_by(doc_id, nrc)%>%
+        summarize(count = sum(n, na.rm = TRUE))%>%
+        filter(!is.na(nrc))%>%
+        pivot_wider(names_from = "nrc", values_from = count)
+    
+    #graphs <- summary %>% 
+     #   rowid_to_column() %>%
+      #  mutate(table = list(rbind(rep(250,10), rep(0,10), summary[rowid, 2:11])),
+       #        graph = radarchart(data.frame(table)))
+    
+    for(i in summary.nrow()){
+        graph <- summary %>% 
+            mutate(table = list(rbind(rep(250,10), 
+                                      rep(0,10), 
+                                      summary[i, 2:11])))
+        radarchart(data.frame(graph))
+    }
+}
 
 delay = 0.1
-url_list <- tibble(url = c("https://www.bis.org/publ/qtrpdf/r_qt1912a.htm",
+url_df <- tibble(url = c("https://www.bis.org/publ/qtrpdf/r_qt1912a.htm",
               "https://www.bis.org/publ/qtrpdf/r_qt2012a.htm"))
 
-url_list <- url_list %>%
-    mutate(text = map_chr(url, request_page, delay = 0.5))
+bis_text <- url_df %>%
+    mutate(text = map_chr(url, request_page, delay = delay))
+
+bis_word_tokens <- unnest_tokens(bis_text, word_tokens, text, token = "words") %>%
+    mutate(report_date = ymd(paste0("20", str_extract(url, "\\d{2}(?=\\d)"), ".",
+                                   str_extract(url, "\\d{2}(?!\\d)"),".1")))%>%
+    select(report_date, word_tokens)
+
+# analyze sentiments
+bis_udp <- udpipe(bis_text$text, "english")
+
+bis_udp$stem <- wordStem(bis_udp$token, language = "porter")
+
+bis_udp_output <- select(bis_udp, "doc_id", "token", "stem", "lemma", "upos") %>%
+    filter(!upos %in% c("PART", "PUNCT", "CCONJ", "SYM", "NUM", "ADP", "AUX", "DET", "PRON", "X", "SCONJ"))
+
+bis_udp_no_stop_words <- bis_udp_output %>% 
+    anti_join(stop_words,by = c("token" = "word"), copy = TRUE) %>% 
+    group_by(doc_id)%>%
+    count(lemma, sort = TRUE)
+
+for(s in c("nrc", "afinn", "bing")){
+    bis_udp_no_stop_words <- bis_udp_no_stop_words%>%
+        left_join(get_sentiments(s), by = c("lemma" = "word")) %>%
+        plyr::rename(replace = c(sentiment = s, value = s), warn_missing = FALSE)
+}
+
+generate_summary_plots
+
+generate_radar_plots(bis_udp_no_stop_words)
 
 
+
+ggplot(data = filter(bis_udp_no_stop_words, !is.na(bing)))+
+    geom_histogram(aes(bing), stat = "count") +
+    scale_x_discrete(guide = guide_axis(angle = 45)) +
+    labs(title = "BIS Sentiment (Bing)")
+
+ggplot(data = filter(bis_udp_no_stop_words, !is.na(nrc)))+
+    geom_histogram(aes(nrc), stat = "count") +
+    scale_x_discrete(guide = guide_axis(angle = 45)) +
+    labs(title = "BIS Sentiment (NRC)")
+
+
+
+    summary <- bis_udp_no_stop_words %>%
+        group_by(doc_id, nrc)%>%
+        summarize(count = sum(n, na.rm = TRUE))%>%
+        filter(!is.na(nrc))%>%
+        pivot_wider(names_from = "nrc", values_from = count)
+    
+    
+    graphs <- summary %>% 
+        mutate(
+            table = list(rbind(rep(250,10) , rep(0,10), .[1])),
+               graph = radarchart(table))
+    
+    
+    graphs <- summary %>% 
+        rowid_to_column()%>%
+        mutate(table = list(rbind(rep(250,10), rep(0,10), summary[rowid, 2:11])),
+               graph = radarchart(data.frame(table)))
+graphs$table
+    
