927d92822584dd247b1258cb9382d4276331549c
jcinterrante <jcinterrante@uchicago.edu>
2021-02-21 20:31:47 -0600

    Added summary statistics, removed not-negation

--- question1.R
+++ question1.R
@@ -3,6 +3,7 @@ library(tidytext)
 library(SnowballC)
 library(udpipe)
 library(fmsb)
+library(scales)
 
 
 data <- read_file("december_2020_bis.txt")
@@ -35,43 +36,68 @@ getStemLanguages()
 bis_udp <- udpipe(data, "english")
 bis_udp$stem <- wordStem(bis_udp$token, language = "porter")
 
-not_list <- bis_udp %>%
-  filter(str_to_lower(lemma) == "not")
+
+# I considered negating "not" but I realized that in the cases in which
+# it was used, the phrases usually didn't negate the sentiment. eg:
+# "this not only puts a floor under short-term rates but also 
+# limits the potential decline." The "not" shouldn't negate
+# the sentiment of the phrase.
+
+#not_list <- bis_udp %>%
+ # cbind_dependencies(type = "parent_rowid", recursive = TRUE)%>%
+  #filter(str_to_lower(token) == "not")
 
 bis_udp_output <- select(bis_udp, "doc_id", "term_id", "token", "stem", "lemma", "upos") %>%
   filter(!upos %in% c("PART", "PUNCT", "CCONJ", "SYM", "NUM", "ADP", "AUX", "DET", "PRON", "X", "SCONJ")) %>%
   mutate_if(is.character, str_to_lower)
 
-# Remove stop words, and also remove the words "debt" and "outstanding." I found
-# that these terms were highly influential on the sentiment scores, even though
-# they were being used in a purely technical, value-neutral sense. "Debt" in the
-# sense of "sovereign debt" can be bad if it is unsustainable, but it can also
-# be good, as in the case of stimulus spending. Similarly, "outstanding" arises
-# in the context of "outstanding obligations," rather than as a synonym for
-# "good."
+# Remove stop words, and also remove the words "debt," "asset", and "outstanding." 
+# I found that these terms were highly influential on the sentiment scores, even though
+# they were being used in a purely technical, value-neutral sense.
 
-bis_udp_no_stop_words <- bis_udp_output %>%
+bis_udp_word_filters <- bis_udp_output %>%
   anti_join(stop_words, by = c("lemma" = "word")) %>%
   count(lemma, sort = TRUE) %>%
-  filter(!lemma %in% c("debt", "outstanding"))
+  filter(!lemma %in% c("debt", "outstanding", "asset"))
 
 for (s in c("nrc", "afinn", "bing")) {
-  bis_udp_no_stop_words <- bis_udp_no_stop_words %>%
+  bis_udp_word_filters <- bis_udp_word_filters %>%
     left_join(get_sentiments(s), by = c("lemma" = "word")) %>%
     plyr::rename(replace = c(sentiment = s, value = s), warn_missing = FALSE)
 }
 
-bis_udp_no_stop_words <- bis_udp_no_stop_words %>%
+bis_udp_word_filters <- bis_udp_word_filters %>%
   mutate(bing = if_else(bing == "positive", 1, -1))
 
-summary(bis_udp_no_stop_words)
-
-bing_graph_data <- bis_udp_no_stop_words %>%
+bing_summary <- bis_udp_word_filters %>%
   group_by(lemma, n, bing) %>%
   summarize() %>%
   mutate(bing_score = n * bing)
 
-ggplot(filter(bing_graph_data, abs(bing_score) > 1)) +
+afinn_summary <- bis_udp_word_filters %>%
+  group_by(lemma, n, afinn) %>%
+  summarize() %>%
+  mutate(afinn_score = n * afinn)
+
+nrc_summary <- bis_udp_word_filters %>%
+  group_by(nrc) %>%
+  summarize(count = sum(n, na.rm = TRUE)) %>%
+  filter(!is.na(nrc)) %>%
+  pivot_wider(names_from = "nrc", values_from = count) %>%
+  select(positive, joy, anticipation, disgust, anger, negative, sadness, fear, surprise, trust)
+
+# Summary Statistics
+print("NRC: Sentiment Proportions")
+nrc_summary%>%pivot_longer(everything(),names_to = "sentiment")%>%
+  mutate(value = value/sum(value)) %>%
+  arrange(-value)%>%
+  mutate(value = percent(value))
+
+summary(select(afinn_summary, afinn_score))
+summary(select(bing_summary, bing_score))
+
+#graphs
+ggplot(filter(bing_summary, abs(bing_score) > 1)) +
   geom_bar(aes(
     y = reorder(lemma, bing_score), x = bing_score,
     fill = factor(bing_score > 0)
@@ -84,15 +110,21 @@ ggplot(filter(bing_graph_data, abs(bing_score) > 1)) +
   ) +
   theme(legend.position = "none")
 
-summary <- bis_udp_no_stop_words %>%
-  group_by(nrc) %>%
-  summarize(count = sum(n, na.rm = TRUE)) %>%
-  filter(!is.na(nrc)) %>%
-  pivot_wider(names_from = "nrc", values_from = count) %>%
-  select(positive, joy, anticipation, disgust, anger, negative, sadness, fear, surprise, trust)
+ggplot(filter(afinn_summary, abs(afinn_score) > 1)) +
+  geom_bar(aes(
+    y = reorder(lemma, afinn_score), x = afinn_score,
+    fill = factor(afinn_score > 0)
+  ),
+  stat = "identity"
+  ) +
+  labs(
+    title = "Overall Contribution to Sentiment Score by Word (Afinn)",
+    x = "Afinn Score", y = ""
+  ) +
+  theme(legend.position = "none")
 
 # https://www.r-graph-gallery.com/142-basic-radar-chart.html
-data <- rbind(rep(250, 10), rep(0, 10), summary)
+data <- rbind(rep(250, 10), rep(0, 10), nrc_summary)
 radarchart(data,
   pfcol = rgb(0.2, 0.5, 0.5, 0.4),
   pcol = rgb(0.2, 0.5, 0.5, 0.9),
@@ -101,5 +133,5 @@ radarchart(data,
   axistype = 1,
   cglcol = "grey", cglty = 1, cglwd = 0.8, caxislabels = seq(0, 250, 62.5),
   title = "Sentiments in the December 2020 BIS (NRC)",
-  vlcex = 1
+  vlcex = .8
 )
